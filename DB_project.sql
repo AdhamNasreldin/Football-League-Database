a0b1f50ca157eb192172fb22f555bf8b685fb880
Zeyad Anwar <zeyad.adlan@gmail.com>
2025-05-16 19:00:57 +0300

    fixed code errors (SQL Server not mySQL)

--- DB_project.sql
+++ DB_project.sql
@@ -8,6 +8,8 @@ DROP TABLE IF EXISTS Team;
 DROP TABLE IF EXISTS Judge;
 DROP TABLE IF EXISTS Stadium;
 DROP TABLE IF EXISTS Person;
+DROP TABLE IF EXISTS Score;
+
 -- Create Person table (parent entity)
 CREATE TABLE Person (
     SSN INT NOT NULL,
@@ -19,11 +21,12 @@ CREATE TABLE Person (
 );
 -- Create Team table
 CREATE TABLE Team (
-    TeamID INT PRIMARY KEY AUTO_INCREMENT,
+    TeamID INT IDENTITY(1,1),
     Name VARCHAR(50) NOT NULL,
     Coach_Salary DECIMAL(10, 2),
     CID INT NoT NULL,
-    FOREIGN KEY (CID) REFERENCES Person(SSN)
+    FOREIGN KEY (CID) REFERENCES Person(SSN),
+	PRIMARY KEY(TeamID)
 );
 -- Create Judge table (subtype of Person)
 CREATE TABLE Judge (
@@ -38,34 +41,40 @@ CREATE TABLE Player (
     Weight DECIMAL(5, 2),
     TeamID INT,
     FOREIGN KEY (PlayerID) REFERENCES Person(SSN),
-    FOREIGN KEY (TeamID) REFERENCES Team(TeamID) on update cascade,
+    FOREIGN KEY (TeamID) REFERENCES Team(TeamID) 
+	ON UPDATE CASCADE
     ON DELETE
     SET NULL
 );
 -- Create Supporter table
 CREATE TABLE Supporter (
-    SupporterID INT PRIMARY KEY AUTO_INCREMENT,
+    SupporterID INT PRIMARY KEY IDENTITY(1,1),
     TeamID INT,
     FOREIGN KEY (TeamID) REFERENCES Team(TeamID)
 );
 -- Create Stadium table
 CREATE TABLE Stadium (
-    SID INT PRIMARY KEY AUTO_INCREMENT,
+    SID INT PRIMARY KEY IDENTITY(1,1),
     Sname VARCHAR(100) NOT NULL,
     Location VARCHAR(200),
     Capacity INT NOT NULL
 );
 -- Create Match table
 CREATE TABLE Match (
-    MatchID INT PRIMARY KEY AUTO_INCREMENT,
+    MatchID INT PRIMARY KEY IDENTITY(1,1),
+	Home INT,
+	Away INT,
     MDate DATE,
     Sname VARCHAR(100),
+	SID INT,
     JudgeID INT,
     HomeTeam INT NOT NULL,
     AwayTeam Int NOT NULL,
     Winning INT,
-    -- NULL m3naha el match 5ls ta3adol 
-    FOREIGN Key (Home) REFERENCES (TeamID) FOREIGN Key (Away) REFERENCES (TeamID) FOREIGN KEY (Sname) REFERENCES Stadium(Sname),
+    -- NULL Means Draw
+    FOREIGN Key (Home) REFERENCES Team(TeamID), 
+	FOREIGN Key (Away) REFERENCES Team(TeamID), 
+	FOREIGN KEY (SID) REFERENCES Stadium(SID),
     FOREIGN KEY (JudgeID) REFERENCES Judge(JudgeID)
 );
 -- Create Score Table
@@ -93,5 +102,5 @@ CREATE TABLE Attends (
     MatchID INT,
     PRIMARY KEY (SupporterID, MatchID),
     FOREIGN KEY (SupporterID) REFERENCES Supporter(SupporterID),
-    FOREIGN KEY (MatchID) REFERENCES Matches(MatchID)
+    FOREIGN KEY (MatchID) REFERENCES Match(MatchID)
 );
